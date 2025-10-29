7b2c7f07d8d5a260f4322fa2c5f0fb7c0bfd1fa7
jamtgard <jamtgardsimon@gmail.com>
2025-10-29 13:44:10 +0100

    Initial commit

---
+++ EdufyUser/src/main/java/com/example/EdufyUser/EdufyUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.EdufyUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdufyUserApplication.class, args);
+	}
+
+}
---
+++ EdufyUser/src/test/java/com/example/EdufyUser/EdufyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EdufyUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
