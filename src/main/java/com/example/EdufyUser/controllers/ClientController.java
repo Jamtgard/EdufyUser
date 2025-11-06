b44cf776835e9766b669cda8522b17898b76a7cc
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-06 13:14:15 +0100

    updated user and added ClientController

---
+++ src/main/java/com/example/EdufyUser/controllers/ClientController.java
@@ -0,0 +1,12 @@
+package com.example.EdufyUser.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//ED-89-AA
+@RestController
+@RequestMapping("/api/v1/user")
+public class ClientController {
+
+
+}
