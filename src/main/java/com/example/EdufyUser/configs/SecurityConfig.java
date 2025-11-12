734fcb518c17dc39bf92583847638bd361583243
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-12 19:31:21 +0100

    connected to keycloak and updated data.sql (removed history)

--- src/main/java/com/example/EdufyUser/configs/SecurityConfig.java
+++ src/main/java/com/example/EdufyUser/configs/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.example.EdufyUser.configs;
 
+import com.example.EdufyUser.convertes.JwtAuthConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,9 +12,16 @@ import org.springframework.security.web.SecurityFilterChain;
 //ED-47-SJ
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-    // Add Converter
+    //ED-223-AA
+    private JwtAuthConverter jwtAuthConverter;
+
+    @Autowired
+    public void setJwtAuthConverter(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,14 +31,12 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/h2-console/**").permitAll()
-                                .anyRequest().permitAll() // Change to Authorized later
-                );
-        /*
+                                .anyRequest().authenticated()
+                )
                 .oauth2ResourceServer(oauth2 ->
                         oauth2
-                                .jwt(jwt -> jwt.jwtAuthenticationConverter())
+                                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
                 );
-         */
         return http.build();
     }
 }
