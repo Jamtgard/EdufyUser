e2501f4a10998a8c12996231c35d4a9afdad579b
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-12 13:16:05 +0100

    removed history-methodes in repository

--- src/main/java/com/example/EdufyUser/repositories/UserRepository.java
+++ src/main/java/com/example/EdufyUser/repositories/UserRepository.java
@@ -18,23 +18,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     //ED-88-AA
     Optional<User> findByUuid(String sub);
-
-    //ED-89-AA
-    @Query(value = "SELECT song_history_id FROM song_history_user WHERE user_id = :userId", nativeQuery = true)
-    Set<Long> findSongHistoryIdsByUserId(@Param("userId") Long userId);
-
-    @Query(value = "SELECT album_history_id FROM album_history_user WHERE user_id = :userId", nativeQuery = true)
-    Set<Long> findAlbumHistoryIdsByUserId(@Param("userId") Long userId);
-
-    @Query(value = "SELECT video_clip_history_id FROM video_clip_history_user WHERE user_id = :userId", nativeQuery = true)
-    Set<Long> findVideoClipHistoryIdsByUserId(@Param("userId") Long userId);
-
-    @Query(value = "SELECT video_playlist_history_id FROM video_playlist_history_user WHERE user_id = :userId", nativeQuery = true)
-    Set<Long> findVideoPlaylistHistoryIdsByUserId(@Param("userId") Long userId);
-
-    @Query(value = "SELECT pod_episode_history_id FROM pod_episode_history_user WHERE user_id = :userId", nativeQuery = true)
-    Set<Long> findPodEpisodeHistoryIdsByUserId(@Param("userId") Long userId);
-
-    @Query(value = "SELECT pod_season_history_id FROM pod_season_history_user WHERE user_id = :userId", nativeQuery = true)
-    Set<Long> findPodSeasonHistoryIdsByUserId(@Param("userId") Long userId);
 }
